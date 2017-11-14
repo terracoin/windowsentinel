3ff6c1a321600ea504d8e35957080e341da4b933
ZonnCash <zonncash@gmail.com>
2017-11-14 22:13:00 +0100

    Allow users to view errors before exitting

--- lib/init.py
+++ lib/init.py
@@ -82,21 +82,40 @@ def has_desire_conf():
 def main():
     install_instructions = "\tpip install -r requirements.txt"
 
+    try: input = raw_input
+    except NameError: pass
+
     if not is_valid_python_version():
         print("Python %s is not supported" % python_short_ver_str())
+
+        print('Press [ENTER] to exit')
+        input()
+
         sys.exit(1)
 
     if not are_deps_installed():
         print("Please ensure all dependencies are installed:")
         print(install_instructions)
+
+        print('Press [ENTER] to exit')
+        input()
+
         sys.exit(1)
 
     if not is_database_correctly_configured():
         print("Please ensure correct database configuration.")
+
+        print('Press [ENTER] to exit')
+        input()
+
         sys.exit(1)
 
     if not has_desire_conf():
         print("DesireCore must be installed and configured, including JSONRPC access in desire.conf")
+        
+        print('Press [ENTER] to exit')
+        input()
+
         sys.exit(1)
 
 
